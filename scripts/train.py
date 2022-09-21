22d8bc0c05e87834b3b0b81110b92ab7a0eeea20
Tegisty Hailay <tigisthay13@gmail.com>
2022-09-21 22:00:53 +0300

    datawarehouse

---
+++ scripts/train.py
@@ -0,0 +1,10 @@
+with open('results.txt', 'w') as result:
+      result.write('Data Warehouse \n')
+      result.write('\n')     
+      result.write('Airflow\n')
+      result.write('\n')
+      result.write(' DAG\n')
+      result.write('\n')  
+      result.write('DBT\n')
+      result.write('\n')
+       
